6063c17f3ed561e35112a516f40841bb69a09a20
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-07-13 18:57:31 -0500

    Build Folder

---
+++ build/precache-manifest.130d6ab9ccc64dd070d4257f2f665ca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f66067ccd2075a3b448ca0702f0b49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630bbae8c92845c7cda1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240303912ba9da13740",
+    "url": "/static/js/2.0feb8754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0feb8754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630bbae8c92845c7cda1",
+    "url": "/static/js/main.84e7121f.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
