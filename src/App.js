6488c9f0058fa6a1b1bb2d1f9fe190053636f59d
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-07-16 11:08:10 -0500

    Added Basic Files

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Route exact path="/" component={Logo} />
+      <Route exact path="/" component={About} />
+      <Route exact path="/work" component={Work} />
+      <Route exact path="/portfolio" component={Portfolio} />
+
     </div>
   );
 }
