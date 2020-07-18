d818503cd08f69dfad7ab76a9ada3759cee55db7
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-07-18 10:02:47 -0500

    One more change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-      <Router basename="/">
+      <Router basename="/React-Portfolio">
         <Container fluid>
           <NavBar />
           <Route exact path="/home" component={Home} />
