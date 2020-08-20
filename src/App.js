1a97b19a006e01ae265dc37c14acdc4a3360cc2d
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-08-20 17:35:45 -0500

    Files Reset

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-
-import NavBar from "./components/NavBar";
-
-import Home from "./components/pages/Home";
-import Work from "./components/pages/Work";
-import Portfolio from "./components/pages/Portfolio";
-
-import Footer from "./components/Footer";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-      <Router basename="/React-Portfolio">
-        <Container fluid>
-          <NavBar />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/work" component={Work} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Footer />
-        </Container>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
