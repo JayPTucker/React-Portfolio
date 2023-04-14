5ecb400b058385d0d45ad77fdd2e4ae6090088c9
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-04-14 16:17:27 -0500

    Updated Home.css
    -Social Media icons stay on right and left side

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,16 +8,23 @@ const Home = () => {
   return (
 
       <Row className="FrontPageRow">
+
         <Col className="info-panel-left" md={1}>
-          <p>test</p>
+          <p>Github</p>
+          <p>Linkedin</p>
+          <p>Gmail</p>
+          <p>Instagram</p>
+
         </Col>
-        <Col md={10}> 
+
+        <Col md={10} className="FrontPageMainCol"> 
         
           <p>Hey there, my name is</p>
           <p>Jay Paul Tucker.</p>
           <p>I'm a passionate Full-Stack Developer specializing in Front-End and Back-End languages. Currently working at Apple as a Technical Expert, I'm dedicated to delivering innovative solutions. Continuously advancing my skills with IBM and Google to stay ahead in the ever-evolving field of software development.</p>
 
         </Col>
+
         <Col className="info-panel-right" md={1}>
           <p>test</p>
         </Col>
