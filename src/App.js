a29ab2dce74d73c35880f698192c7f95a87fafd6
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-04-14 00:48:54 -0500

    Fresh Branch Uploaded
    Test Branch to redo Website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-
 import './App.css';
-
-import Navbar from './components/Navbar';
-import HomePage from './components/pages/Home';
-import AboutPage from './components/pages/About'; // import the AboutPage component
-import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <Container className='mainContainer' fluid>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/about' component={AboutPage} />
-        </Switch>
-        <Footer />
-      </Container>
-    </Router>
+    <div>Jay Paul Tucker</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
-
 import React from 'react';
-import { createRoot } from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
-createRoot(document.getElementById('root')).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
