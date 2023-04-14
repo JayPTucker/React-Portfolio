9d3479ec8db6d40525a7e9903485742680562f63
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-04-14 13:58:30 -0500

    Added Navbar.css
    Added in the Navbar CSS from the devTest branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Navbar from './pages/Navbar';
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,22 +1,29 @@
-
 import React from 'react';
+import { Row, Col } from "react-bootstrap";
+
+// Even though Router, Routes, Route, and Link are not used in this file, they are needed for the app to work
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        {/* Add navigation links */}
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  <Row className="navbar sticky-top fixed-top" id="navbar">
+    <Col md={3}>
+        <p className="navbar-title">Jay Paul Tucker</p>
+    </Col>
+
+    <Col md={9} className="navbar-links-col">
+        <a className="navbar-link" href="/">Home</a>
+        <a className="navbar-link" href="/#work">Work</a>
+        <Link to="/about" className="navbar-link">About</Link>
+        <a className="navbar-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1EJdzVG4qJjfpTwqXrnh_knmB5JlADeye/view?usp=sharing">Resume</a>
+    </Col>
+
+    {/* <a href="mailto:jaypaultucker@gmail.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="shortcut-icon mail-icon" icon={faEnvelope} /></a>
+    <a href="https://github.com/JayPTucker" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="shortcut-icon github-icon" icon={faGithub} /></a>
+    <a href="https://www.linkedin.com/in/jayptucker/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="shortcut-icon linkedin-icon" icon={faLinkedin} /></a> */}
+
+  </Row>
   );
 };
 
