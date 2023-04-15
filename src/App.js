de7725c6bb83b049a8423cc3ae839c44cdfef43a
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-04-15 13:14:39 -0500

    Updated Loading Screen
    - Made separate file for Loading Screen and routed it to App.js
    - Updated CSS for loading screen.
    - Will be adding in proper animation to app loading soon

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-// src/App.js
-
-import React, {useEffect} from 'react';
+// =========================================
+// IMPORTS:
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/App.css';
+// =========================================
+
+// =========================================
+// PAGES
 import Home from './pages/Home';
 import About from './pages/About';
 import Navbar from './pages/Navbar';
+import LoadingScreen from "./pages/LoadingScreen.js";
+// =========================================
 
-import { Container } from 'react-bootstrap';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
-import './styles/App.css';
+
+// =========================================
 
 const App = () => {
+
+  // ================================
+  // Function for the loading screen
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate data loading or other async tasks
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+  
+  // ================================
+
   return (
+    
     <Router>
-      <Container className="mainContainer" fluid>
-        <Navbar></Navbar>
-        {/* Define routes and components */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
+      {/* Implementation of loading screen */}
+      {isLoading ? (
+      <LoadingScreen /> // Display the loading screen if isLoading is true
+      ) : (
+
+        <Container className="mainContainer" fluid>
+          <Navbar></Navbar>
+          {/* Define routes and components */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Container>
+      )}
     </Router>
   );
 };
