84f377a4e10f5b1f82abeee77be9ef9d25209bc4
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-10-10 13:55:43 -0500

    Update greenscreen component
    - Added alert warning for users that are using Safari
    - Fixed error logs

--- src/components/greenscreen.js
+++ src/components/greenscreen.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GreenScreenVid from '../img/GSFffm.webm'
-
-function isSafari() {
-    return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-}  
 
 function GreenScreenComponent() {
     // Check if the user is using Safari
     const isUsingSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+    if (isUsingSafari === true) {
+        alert("This Website does not support Safari - Some features may not work as intended.  Please use Chrome or any other browser.")
+    } else {
+        return (
+        
+            <div>
+              {/* Other content */}
+              {isUsingSafari ? (
+                <p></p>
+              ) : (
+                <video autoPlay loop muted className="GreenScreenVid" alt="Gif of me">
+                  {/* <source src={GreenScreenGif} type="video/webm" /> */}
+                  <source src={GreenScreenVid} type="video/webm" />
+                  {/* Add additional source elements for different video formats if needed */}
+                </video>
+              )}
+              {/* Other content */}
+            </div>
+        );
+    }
   
-    return (
-      <div>
-        {/* Other content */}
-        {isUsingSafari ? (
-          <p></p>
-        ) : (
-          <video autoPlay loop muted className="GreenScreenVid" alt="Gif of me">
-            {/* <source src={GreenScreenGif} type="video/webm" /> */}
-            <source src={GreenScreenVid} type="video/webm" />
-            {/* Add additional source elements for different video formats if needed */}
-          </video>
-        )}
-        {/* Other content */}
-      </div>
-    );
+
   }
   
 export default GreenScreenComponent;
